b0c52fffeff8682895eb819f20761c897303614d
chiranjeevi.macharla <chiranjeevi.macharla@oroprise.com>
2018-02-15 14:44:37 +0530

    Initial Commit

---
+++ SampleTestFW/SampleTestFW.h
@@ -0,0 +1,19 @@
+//
+//  SampleTestFW.h
+//  SampleTestFW
+//
+//  Created by Muttavarapu on 15/02/18.
+//  Copyright © 2018 Muttavarapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleTestFW.
+FOUNDATION_EXPORT double SampleTestFWVersionNumber;
+
+//! Project version string for SampleTestFW.
+FOUNDATION_EXPORT const unsigned char SampleTestFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleTestFW/PublicHeader.h>
+
+
