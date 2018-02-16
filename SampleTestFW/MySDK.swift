a30d55dd9c97887c79bffa32a292de818c19e897
chiranjeevi.macharla <chiranjeevi.macharla@oroprise.com>
2018-02-16 11:23:19 +0530

    first commit

---
+++ SampleTestFW/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MyFIle.swift
+//  SampleTestFW
+//
+//  Created by Muttavarapu on 15/02/18.
+//  Copyright © 2018 Muttavarapu. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
